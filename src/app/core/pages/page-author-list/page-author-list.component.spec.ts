f292b53a1aec6320ef17eaa19eed983c49a87fc7
Piotr Kowalski <piecioshka@gmail.com>
2019-03-03 13:16:47 +0100

    Fix import

--- src/app/core/pages/page-author-list/page-author-list.component.spec.ts
+++ src/app/core/pages/page-author-list/page-author-list.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SharedModule } from '../../../shared/shared.module';
-import { PageAuthorListComponent } from './author-list.component';
+import { PageAuthorListComponent } from './page-author-list.component';
 
 describe('PageAuthorListComponent', () => {
     let component: PageAuthorListComponent;
