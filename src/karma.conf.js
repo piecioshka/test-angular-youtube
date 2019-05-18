662a4b4cc577dff605f4a09402b670bb3e368426
Piotr Kowalski <piecioshka@gmail.com>
2019-05-18 13:00:39 +0200

    Karma: ChromeHeadless

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -24,8 +24,8 @@ module.exports = function (config) {
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
-        autoWatch: false,
-        browsers: ['Chrome'],
+        autoWatch: true,
+        browsers: ['ChromeHeadless'],
         singleRun: false
     });
 };
